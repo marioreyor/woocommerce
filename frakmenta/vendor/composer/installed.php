50a46d323ecf8a20aed704cc4f8e90520e43d48c
Mario_Rey <mario_rey@findirect.com>
2024-01-15 15:49:15 +0100

    Shift to php 8.2

---
+++ frakmenta/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ba2d619bee7b09eb4364371065db7e815cd218f1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ba2d619bee7b09eb4364371065db7e815cd218f1',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.56',
+            'version' => '1.10.56.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '27816a01aea996191ee14d010f325434c0ee76fa',
+            'dev_requirement' => true,
+        ),
+        'rector/rector' => array(
+            'pretty_version' => '0.19.0',
+            'version' => '0.19.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rector/rector',
+            'aliases' => array(),
+            'reference' => '503f4ead06b3892bd106f67f3c86d4e36c94423d',
+            'dev_requirement' => true,
+        ),
+    ),
+);
